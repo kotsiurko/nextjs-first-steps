6af21b1a3ca2961d923b521aac0312dd4a3d79c0
Roman Kotsiurko <kotsiurko@gmail.com>
2023-02-26 15:17:25 +0200

    add dynamic routes

---
+++ pages/posts/[id].js
@@ -0,0 +1,62 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
+
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+// export default function Post({ postData }) {
+//   console.log(postData.title);
+//   return (
+//     <Layout>
+//       {/* Add this <Head> tag */}
+//       <Head>
+//         <title>{postData.title}</title>
+//       </Head>
+//       {/* <b>Post Title:</b> {postData.title} */}
+//       {/* <br /> */}
+//       <b>ID:</b> {postData.id}
+//       <br />
+//       <b>Post number:</b> {postData.postID}
+//       <br />
+//       <b>Published:</b> <Date dateString={postData.date} />
+//       <br />
+//       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+//     </Layout>
+//   );
+// }
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
